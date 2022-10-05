4a27a0c23592d64e910ddec2abbe62c32a39c2fe
anshu0x <anshusharma6327@gmail.com>
2022-10-05 11:11:35 +0530

    insert element in array

--- InsertEl.js
+++ InsertEl.js
@@ -4,12 +4,19 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8]
 let pos = 2;
 let elm = 34
 
-for (let i = arr.length - 1; i >= 0; i--) {
-    if (i >= pos) {
-        arr[i + 1] = arr[i]
-        if (i == pos) {
-            arr[i] = elm
-        }
-    }
+// for (let i = arr.length - 1; i >= 0; i--) {
+//     if (i >= pos) {
+//         arr[i + 1] = arr[i]
+//         if (i == pos) {
+//             arr[i] = elm
+//         }
+//     }
+// }
+
+
+for (let i = arr.length - 1; i >= pos; i--) {
+    arr[i + 1] = arr[i]
 }
+arr[pos] = elm
+
 console.log(arr);
